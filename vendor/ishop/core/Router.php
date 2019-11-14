811d31d744c5d905d01d06a0ed37e733eaf810ab
Dmitriy Zhirko <zhirkod@gmail.com>
2019-11-14 16:36:35 +0200

    7

--- vendor/ishop/core/Router.php
+++ vendor/ishop/core/Router.php
@@ -4,6 +4,8 @@
 namespace ishop;
 
 
+use mysql_xdevapi\Exception;
+
 class Router {
 
     protected static $routes = [];
@@ -23,15 +25,62 @@ class Router {
 
     public static function dispatch($url){
         if(self::matchRoute($url)){
-            echo 'ok';
+            $controller = 'app\controllers\\' . self::$route['prefix'] .
+            self::$route['controller'] . 'Controller';
+            if(class_exists($controller)){
+                $controllerObject = new $controller(self::$route);
+                $action = self::lowerCamelCase(self::$route['action']) . 'Action';
+                if(method_exists($controllerObject, $action)){
+                    $controllerObject->$action();
+                }else {
+                    throw new \Exception("Метод $controller::$action 
+                    не найден", 404);
+                }
+            }
+            else {
+                throw new \Exception("Контроллер $controller не найден");
+            }
+
         }
         else {
-            echo 'no';
+            throw new \Exception('Стриница не найдена', 404);
         }
     }
 
     public static function matchRoute($url) {
+        foreach(self::$routes as $pattern => $route){
+            if(preg_match("#{$pattern}#", $url, $matches)){
+                foreach ($matches as $k => $v){
+                    if(is_string($k)){
+                        $route[$k] = $v;
+                    }
+                }
+                if(empty($route['action'])){
+                    $route['action'] = 'index';
+                }
+                if(!isset($route['prefix'])){
+                    $route['prefix'] = '';
+                } else {
+                    $route['prefix'] .= '\\';
+                }
+                $route['controller'] = self::upperCamelCase($route['controller']);
+                self::$route = $route;
+                return true;
+            }
+        }
         return false;
+    }
+
+    protected static function upperCamelCase($name){
+        $name = str_replace('-', ' ', $name);
+        $name = ucwords($name);
+        return str_replace(' ', '', $name);
+
+    }
+
+    protected static function lowerCamelCase($name){
+        $name = self::upperCamelCase($name);
+        return lcfirst($name);
     }
 
 }
