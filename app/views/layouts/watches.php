a51eaa2613f442d183cffdbbb2b849e2ed0074ec
Dmitriy Zhirko <zhirkod@gmail.com>
2019-11-17 14:01:10 +0200

    22

--- app/views/layouts/watches.php
+++ app/views/layouts/watches.php
@@ -71,7 +71,7 @@ License URL: http://creativecommons.org/licenses/by/3.0/
             <div class="col-md-9 header-left">
                 <div class="menu">
                     <?php new \app\widgets\menu\Menu([
-                            'tpl' => WWW . '/menu/menu.php',
+                            'tpl' => WWW . '/menu/menu.php'
                     ]);?>
                 </div>
 <!--                <div class="top-nav">-->
