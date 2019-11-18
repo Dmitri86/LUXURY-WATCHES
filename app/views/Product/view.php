addbdead4a735d2e55f777047e1f467078ad9299
Dmitriy Zhirko <zhirkod@gmail.com>
2019-11-18 12:25:09 +0200

    27

--- app/views/Product/view.php
+++ app/views/Product/view.php
@@ -14,7 +14,6 @@
 <div class="single contact">
     <div class="container">
         <div class="single-main">
-
             <div class="col-md-9 single-main-left">
                 <div class="sngl-top">
                     <div class="col-md-5 single-top-left">
