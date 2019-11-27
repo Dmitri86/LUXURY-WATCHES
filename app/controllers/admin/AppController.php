d4ad51d48e8aee87346e8881ef28b496befcbf8d
Dmitriy Zhirko <zhirkod@gmail.com>
2019-11-27 17:11:45 +0200

    58

--- app/controllers/admin/AppController.php
+++ app/controllers/admin/AppController.php
@@ -22,4 +22,17 @@ class AppController extends Controller {
         new AppModel();
     }
 
+    public function getRequestID($get = true, $id = 'id'){
+        if($get){
+            $data = $_GET;
+        }else{
+            $data = $_POST;
+        }
+        $id = !empty(($id)) ? (int)$data[$id] : null;
+        if(!$id){
+            throw new \Exception('Странница не нацденна', 404);
+        }
+        return $id;
+    }
+
 }
