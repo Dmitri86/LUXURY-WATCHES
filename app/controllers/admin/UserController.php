246fcd518cf4cb66924a192ba929875050260f4c
Dmitriy Zhirko <zhirkod@gmail.com>
2019-11-30 15:29:24 +0200

    66

--- app/controllers/admin/UserController.php
+++ app/controllers/admin/UserController.php
@@ -5,8 +5,21 @@ namespace app\controllers\admin;
 
 
 use app\models\User;
+use ishop\libs\Pagination;
+use RedBeanPHP\R;
 
 class UserController extends AppController{
+
+    public function indexAction(){
+        $page = isset($_GET['page']) ? (int)$_GET['page'] : 1;
+        $perpage = 3;
+        $count = R::count('user');
+        $pagination = new Pagination($page, $perpage, $count);
+        $start = $pagination->getStart();
+        $users = R::findAll('user', "LIMIT $start, $perpage");
+        $this->setMeta('Список пользователей');
+        $this->set(compact('users', 'pagination', 'count'));
+    }
 
     public function loginAdminAction(){
         if(!empty($_POST)){
@@ -23,6 +36,13 @@ class UserController extends AppController{
 
         $this->layout = 'login';
 
+    }
+
+    public function editAction(){
+        $user_id = $this->getRequestID();
+        $user = R::load('user', $user_id);
+        $this->setMeta('Редактирование профиля пользователя');
+        $this->set(compact('user'));
     }
 
 
